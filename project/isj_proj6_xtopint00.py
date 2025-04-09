5eb9ab1dd06c01ef123b8e534d8901292541f86e
Lander456 <tadeas.topinka@gmail.com>
2025-04-09 23:32:17 +0200

    isj_proj6 [FEATURE] added the latest project

---
+++ project/isj_proj6_xtopint00.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+from collections import Counter
+import doctest
+
+class ValidationError(Exception):
+
+    pass
+
+def upd_word_counts(sentence, word_counts = None, *, to_upper = False):
+
+    if word_counts is None:
+
+        word_counts = Counter()
+
+    internalCounter = word_counts.copy()
+
+    if to_upper:
+
+        for key in word_counts.keys():
+
+            if not key.isupper():
+
+                raise ValidationError()
+
+        sentence = sentence.upper()
+
+    internalCounter.update(sentence.split())
+
+    return internalCounter
+
+
+if __name__ == "__main__":
+
+    doctest.testmod()
